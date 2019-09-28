26bd8bc7fb0b3452a35d2683e9bbe3b2ae4bd73c
wajd-mariam <54957090+wajd-mariam@users.noreply.github.com>
2019-09-27 21:32:42 -0400

    Update pybadge_background.py

--- pybadge_background.py
+++ pybadge_background.py
@@ -10,24 +10,25 @@ import stage
 # an image bank of CircuitPython
 image_bank_1 = stage.Bank.from_bmp16("ball.bmp")
 
+
 def main():
 
     # sets the background to image 0 in the bank
     background = stage.Grid(image_bank_1, 10, 8)
     # changing 0,0 iamge to the 3rd image
-    background.tile(4 ,3 , 3)
-   
+    background.tile(4, 3, 3)
+
     # setting up the background to show up on the PyPadge
     # setting up the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
     game.layers = [background]
-   
-   
+
     game.render_block()
-   
+
     while True:
         # comment
         pass
-       
+
+
 if __name__ == "__main__":
     main()
